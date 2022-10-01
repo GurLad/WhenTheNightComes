914f41c84d8a3a8d08d5a04c9d72e93d82d4f181
RaddiPL <44931681+RaddiPL@users.noreply.github.com>
2022-10-01 21:03:08 +0200

    added some game UI

---
+++ Assets/Scripts/UIClock.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIClock : MonoBehaviour
+{
+    private float ClockTime, LastTime;
+    public GameObject MinHandle, HourHandle;
+    public bool ClockTicking;
+
+    private SettingsController SC;
+
+    void Start()
+    {
+        ClockTime = 0;
+        SC = FindObjectOfType<SettingsController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!SC.SettingsActive)
+        {
+            LastTime = ClockTime;
+            if (ClockTicking)
+                ClockTime += Time.deltaTime;
+
+            MinHandle.transform.rotation = Quaternion.Euler(0, 0, -ClockTime % 10.0f * 36);
+
+            HourHandle.transform.rotation = Quaternion.Euler(0, 0, -Mathf.Floor(ClockTime / 10.0f) * 30);
+        }
+    }
+}
