e839cfaf82bcacf79bc8333dd90e2a962dcd8e98
Gur Ladizhinsky <40135047+GurLad@users.noreply.github.com>
2022-10-01 22:05:55 +0200

    Added caretakers

--- Assets/Scripts/Caretaker.cs
+++ Assets/Scripts/Caretaker.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Caretaker : MonoBehaviour
 {
+    public float Speed;
     public float ArrivalThreshold = 0.1f;
+    public float TurnSpeed;
     private Rigidbody rigidbody;
     private List<Vector2Int> currentPath = new List<Vector2Int>();
     private float count;
@@ -24,20 +26,28 @@ public class Caretaker : MonoBehaviour
             if (Vector3.Distance(transform.position - new Vector3(0, transform.position.y, 0), currentPath[0].To3D()) <= ArrivalThreshold)
             {
                 currentPath.RemoveAt(0);
+                if (currentPath.Count <= 0)
+                {
+                    rigidbody.velocity = Vector3.zero;
+                    return;
+                }
                 // Adapted from https://answers.unity.com/questions/1023987/lookat-only-on-z-axis.html
                 Vector3 difference = currentPath[0].To3D() - transform.position;
                 targetRot = Mathf.Atan2(difference.z, difference.x) * Mathf.Rad2Deg;
             }
+            if (Mathf.Abs(targetRot - yRot) >= ArrivalThreshold)
+            {
+                yRot += Mathf.Sign(targetRot - yRot) * Time.deltaTime * TurnSpeed;
+            }
+            else
+            {
+                yRot = targetRot;
+            }
+            transform.localEulerAngles = new Vector3(0, yRot, 0);
+            Vector3 dir = -(transform.position - currentPath[0].To3D());
+            dir.y = 0;
+            rigidbody.velocity = dir.normalized * Speed;
         }
-        if (Mathf.Abs(targetRot - yRot) >= ArrivalThreshold)
-        {
-            yRot -= Mathf.Sign(targetRot - yRot) * Time.deltaTime;
-        }
-        else
-        {
-            yRot = targetRot;
-        }
-        transform.localEulerAngles = new Vector3(0, yRot, 0);
     }
 
     public void SetTarget(Vector2Int pos)
