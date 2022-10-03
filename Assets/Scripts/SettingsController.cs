48ec775c03123d6bf3546869c310b15d2a84809b
Gur Ladizhinsky <40135047+GurLad@users.noreply.github.com>
2022-10-03 11:09:03 +0200

    Added saving settings

--- Assets/Scripts/SettingsController.cs
+++ Assets/Scripts/SettingsController.cs
@@ -18,9 +18,7 @@ public class SettingsController : MonoBehaviour
         {2048,1536},
         {2560,1440},
         {2560,1600},
-        {2560,2048},
-        {4096,2304},
-        {8192,4608}};
+        {2560,2048}};
 
     public bool Fullscreen;
     public int Resolution;
@@ -28,9 +26,35 @@ public class SettingsController : MonoBehaviour
     private int CameraSensitivityUpdated = 0;
     void Start()
     {
+        LoadSettings();
         UpdateSettings();
         Cursor.visible = false;
         UIManager_ = FindObjectOfType<UIManager>();
+    }
+
+    private void LoadSettings()
+    {
+        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume", 1);
+        MusicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1);
+        SensitivitySlider.value = PlayerPrefs.GetFloat("Sensitivity", 0.6f);
+        FullscreenBox.isOn = PlayerPrefs.GetInt("Fullscreen", 1) > 0;
+        if (!PlayerPrefs.HasKey("Resolution"))
+        {
+            // Find resolution
+            Resolution res = Screen.currentResolution;
+            for (int i = 0; i < Resolutions.Length; i++)
+            {
+                if (Resolutions[i, 0] == res.width && Resolutions[i, 1] == res.height)
+                {
+                    ResolutionDropdown.value = i;
+                    return;
+                }
+            }
+        }
+        else
+        {
+            ResolutionDropdown.value = PlayerPrefs.GetInt("Resolution");
+        }
     }
 
     public void UpdateSettings()
@@ -44,6 +68,14 @@ public class SettingsController : MonoBehaviour
         Screen.fullScreen = Fullscreen;
         Screen.SetResolution(Resolutions[Resolution, 0], Resolutions[Resolution, 1], Fullscreen);
         SoundController.Volume = SFX;
+        CrossfadeMusicPlayer.Instance.Volume = Music;
+
+        // Save
+        PlayerPrefs.SetFloat("SFXVolume", SFX);
+        PlayerPrefs.SetFloat("MusicVolume", Music);
+        PlayerPrefs.SetFloat("Sensitivity", Sensitivity);
+        PlayerPrefs.SetInt("Fullscreen", Fullscreen ? 1 : 0);
+        PlayerPrefs.SetInt("Resolution", Resolution);
     }
 
     private void Update()
@@ -66,8 +98,13 @@ public class SettingsController : MonoBehaviour
     public void ChangeVisibilityOfSettingsMenu()
     {
         if(UIManager_.IsAnyWindowOpen())
+        {
             UIManager_.CloseWindows();
+        }
         else
+        {
             UIManager_.OpenWindow(UIManager.UIElements.Settings);
+            LoadSettings();
+        }
    }
 }
